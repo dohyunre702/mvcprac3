954e0f70d76f3b3ddb11407220d858c5e6531cb3
dohyu <dohyunre702@gmail.com>
2022-11-10 17:41:50 +0900

    first commit

---
+++ src/main/java/com/ex/mvcprac3/Mvcprac3Application.java
@@ -0,0 +1,13 @@
+package com.ex.mvcprac3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvcprac3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mvcprac3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ex/mvcprac3/Mvcprac3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.mvcprac3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvcprac3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
