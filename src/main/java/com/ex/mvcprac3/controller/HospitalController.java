82e3069206afa1fcbe0244528bf97bf4e5fa7f72
dohyu <dohyunre702@gmail.com>
2022-11-11 14:26:44 +0900

    11.11 페이징 (Controller, template)

--- src/main/java/com/ex/mvcprac3/controller/HospitalController.java
+++ src/main/java/com/ex/mvcprac3/controller/HospitalController.java
@@ -2,11 +2,12 @@ package com.ex.mvcprac3.controller;
 
 import com.ex.mvcprac3.domain.Hospital;
 import com.ex.mvcprac3.repository.HospitalRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
+@Slf4j
 @Controller
 @RequestMapping("/hospitals")
 public class HospitalController {
@@ -18,10 +19,13 @@ public class HospitalController {
 
     //get
     //1. list
-    @GetMapping("")  // list일 때 list로
-    public String list(Pageable pageable, Model model){ //리포지토리
+    @GetMapping("") //페이징
+    public String list(Model model, Pageable pageable){ //리포지토리
         Page<Hospital> hospitals = hospitalRepository.findAll(pageable);
+        log.info("size:{}", hospitals.getSize());
         model.addAttribute("hospitals",hospitals);
+        model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());
+        model.addAttribute("next", pageable.next().getPageNumber());
         return "hospitals/list";
     }
 
